c55072895f6cd6693cc8b0785791bcba553d0149
olehratinskiy <oleh.ratinskyi.knm.2020@lpnu.ua>
2023-04-04 02:35:57 +0300

    changed processes distribution

--- main.cpp
+++ main.cpp
@@ -9,6 +9,12 @@ float f(float x) {
     return x - 10 * sin(x) + abs(pow(x, 2) - x * exp(5));
 }
 
+struct Res {
+    float num;
+    float x;
+    float y;
+};
+
 int main(int argc, char* argv[]) {
     int procCount, procNum;
     MPI_Status status;
@@ -24,41 +30,39 @@ int main(int argc, char* argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &procCount);
     MPI_Comm_rank(MPI_COMM_WORLD, &procNum);
 
-    vector <vector<float>> result;
-    vector <vector<float>> currentResult;
+    int count = (int) ((N + 1) / h);
+    Res result[count];
+    Res currentResult[count];
 
     if (procNum == 0) {
         for (int i = 1; i < procCount; i++) {
             MPI_Recv(currentResult, 3, MPI_FLOAT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-            for (int j = 0; j < currentResult.size(); j++)
-                cout << "Process №" << currentResult[j][0] << " f(" << currentResult[j][1] << ") = " << currentResult[j][2] << endl;
+            cout << "Process №" << currentResult[j][0] << " f(" << currentResult[j][1] << ") = " << currentResult[j][2] << endl;
         }
     } else {
-        vector<float> tmpResult;
-
         if (procNum == 1) x = 1 - h;
 
-        for (int i = 0; i < (int) round(((N + 1) / h) / (float) procCount); i++) {
+        for (int i = 0; i < (int) round(((float) count / (float) procCount)); i++) {
             x += h;
             y = f(x);
-            tmpResult.push_back((float) procNum);
-            tmpResult.push_back(x);
-            tmpResult.push_back(y);
-            result.push_back(tmpResult);
-            tmpResult.clear();
+            Res r{};
+            r.num = (float) procNum;
+            r.x = x;
+            r.y = y;
+            result[((procNum - 1) * count) + i] = r;
         }
 
-        if (procNum == procCount - 1) {
-            while (x + h <= N + 1) {
-                x += h;
-                y = f(x);
-                tmpResult.push_back((float) procNum);
-                tmpResult.push_back(x);
-                tmpResult.push_back(y);
-                result.push_back(tmpResult);
-                tmpResult.clear();
-            }
-        }
+//        if (procNum == procCount - 1) {
+//            while (x + h <= N + 1) {
+//                x += h;
+//                y = f(x);
+//                tmpResult.push_back((float) procNum);
+//                tmpResult.push_back(x);
+//                tmpResult.push_back(y);
+//                result.push_back(tmpResult);
+//                tmpResult.clear();
+//            }
+//        }
 
         MPI_Send(result, 3, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
 
